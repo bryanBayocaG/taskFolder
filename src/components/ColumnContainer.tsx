2a62767f45ce8cb2cdf7c2a084e6ca1e225b2384
Bryan Adanza Bayoca <brynbayoca686@gmail.com>
2025-02-06 15:10:28 +0800

    try

--- src/components/ColumnContainer.tsx
+++ src/components/ColumnContainer.tsx
@@ -1,4 +1,6 @@
 import { Column } from "@/type"
+import { Button } from "./ui/button";
+import { FaRegTrashAlt } from "react-icons/fa";
 
 interface Props {
     column: Column;
@@ -7,8 +9,16 @@ interface Props {
 function ColumnContainer(props: Props) {
     const { column } = props;
     return (
-        <div className="border bg-white dark:border-gray-800 dark:bg-gray-950 p-3 p-2 w-[350px] h-[500px] rounded-md flex flex-col">
-            {column.title}
+        <div className="backdrop-blur-[16px] shadow-2xl p-1 w-[350px] h-[500px] rounded-md flex flex-col">
+            <div className="bg-gray-400 dark:bg-gray-900 p-2 rounded-t-md flex justify-between items-center">
+                <div className="flex items-center gap-2">
+                    <div className="flex justify-center items-center bg-white dark:bg-gray-950 px-2 py-1 text-sm rounded-full">0</div>
+                    {column.title}
+                </div>
+                <Button size="icon" variant="ghost">
+                    <FaRegTrashAlt className="text-red-700" />
+                </Button>
+            </div>
             <div className="flex flex-grow">
 
             </div>
