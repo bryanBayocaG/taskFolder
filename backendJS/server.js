7eb129ff2992b0f99c7d7f31a4823959ee51fc3b
Bryan Adanza Bayoca <brynbayoca686@gmail.com>
2025-02-25 23:07:47 +0800

    vercel dep

--- backendJS/server.js
+++ backendJS/server.js
@@ -8,25 +8,24 @@ dotenv.config();
 
 const app = express();
 
-// Connect to database
 connectDB();
 
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://your-frontend.vercel.app"],
+    origin: ["http://localhost:5173"],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true,
   })
 );
-
-// Test Route
 app.get("/", (req, res) => {
-  res.send("Task folder API running on Vercel!");
+  res.send(" task folder api JS");
 });
 
-// API Routes
 app.use("/api/", webRoute);
 
-// Export app for Vercel
-export default app;
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Running at http://localhost:${PORT}`);
+});
